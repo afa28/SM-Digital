f811575e1cbd4fe805a6fdf27da7477bba2b669e
afa28 <andifahruddinakas@gmail.com>
2020-11-27 20:34:23 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '$mfikri#2017)[]\%757qlchdklvjsc;alcvskv.s,/02-ru482wyr7';
+$config['encryption_key'] = 'andifahruddinakas@gmail.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,8 +381,8 @@ $config['encryption_key'] = '$mfikri#2017)[]\%757qlchdklvjsc;alcvskv.s,/02-ru482
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
-$config['sess_save_path'] = NULL;
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
