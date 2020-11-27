a2752dabc099836107395b7495a0e27f02fb3a93
afa28 <andifahruddinakas@gmail.com>
2020-11-27 13:16:01 +0700

    Initial commit

---
+++ application/views/v_about.php
@@ -0,0 +1,200 @@
+
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="M Fikri Setiadi" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">SOE MULTI DIGITAL<span>.</span></a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li class="active"><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Portfolio</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Blog</a></li>
+						<li><a href="<?php echo base_url().'gallery'?>">Gallery</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+						<li class="cta"><a href="<?php echo base_url().'portfolio'?>">Get started</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/slide_3.jpg'?>);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Cari tahu tentang kami</h2>
+		   					<p class="fh5co-lead"> Alamat Web Soe Multi Digital by <a href="http://soemultidigital.id/" target="_blank">Soe Multi Digital</a></p>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div class="fh5co-about animate-box">
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>About Us</h2>
+			<p>Soe Multi Digital adalah sebuah perusahaan yang bergerak dibidang jasa, pelatihan dan penyedia aplikasi dan website dengan menggunakan teknologi tepat guna untuk "Mewujdukan Pelayanan Prima".</p>
+		</div>
+		<div class="container">
+			<div class="col-md-6">
+				<figure>
+					<img src="<?php echo base_url().'theme/images/image_1.jpg'?>" alt="Free HTML5 Template" class="img-responsive">
+				</figure>
+			</div>
+			<div class="col-md-6">
+				<h3>Visi</h3>
+				<ul>
+					<li>Mewujudkan pelayanan prima menggunakan teknologi tepat guna.</li>
+				
+				</ul>
+				<h3>Misi</h3>
+				<ul>
+					<li>Bertakwa Kepada Tuhan Yang Maha Esa</li>
+					<li>Membangun Sistem terintegritasi menggunakan teknologi tepat guna</li>
+					<li>Mewujudkan pengelolaan administrasi yang akurat, tertib dan aman.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="fh5co-team animate-box">
+		<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<h2>Tim Kami</h2>
+						<p>Kami memiliki tim yang solid, bersama kita bisa untuk mewujudkan pelayanan prima menggunakan teknologi tepat guna.</p>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person3.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Sukardi</h3>
+						<h4>Co-Founder, CEO</h4>
+						<p>Sukardi adalah seorang Matematikawan dari lulusan salah satu Universitas terbaik Indonesia yang kini merintis sebuah perusahaan dalam mewujudkan pelayanan prima menggunakan teknologi tepat guna</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person2.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Ruslan</h3>
+						<h4>Co-Founder, CTO</h4>
+						<p>Ruslan lulusan dari salah satu Universitas terbaik Indonesia yang tengah menjalan beberapa usaha diantaranya adalah perusahaan yang bergerak dibidang teknologi dan informasi.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person4.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Abd. Rajab</h3>
+						<h4>Co-Founder, Designer</h4>
+						<p>Abd. Rajab adalah ....</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+	</div>
+
+
+	<div class="fh5co-services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>LAYANAN</h2>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/30.svg'?>" class="img-responsive"></span>
+					<h3>SOFTWARE & PROGRAM</h3>
+					<p>Tingkatkan kinerja perusahaan dengan Software yang sesuai dengan Business Process anda.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/18.svg'?>" class="img-responsive"></span>
+					<h3>IT CONSULTING</h3>
+					<p>Konsultasi kan kebutuhan IT anda pada kami dan ketahui layanan lain yang kami berikan.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/27.svg'?>" class="img-responsive"></span>
+					<h3>WEB DESIGN</h3>
+					<p>Bangun identitas bisnis dan usaha anda di dunia Internet melalui Website.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
